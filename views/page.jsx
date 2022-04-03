4bcd282de63e84c78a8bb1a3d916d63d350ca93c
Andri Möll <andri@dot.ee>
2022-04-03 14:04:02 +0000

    Set viewport width.

--- views/page.jsx
+++ views/page.jsx
@@ -44,7 +44,7 @@ function Page(attrs, children) {
 	return <html lang="en" class={attrs.class}>
 		<head>
 			<meta charset="utf-8" />
-			<meta name="viewport" content="width=device-width" />
+			<meta name="viewport" content="width=1000px" />
 
 			<link
 				rel="stylesheet"
